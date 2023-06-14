ed09da819775a02ed2f7e34f8956e1fa200a2453
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-06-13 18:59:10 -0600

    Result init and props done

--- lib/classes/result.py
+++ lib/classes/result.py
@@ -6,3 +6,17 @@ class Result:
         self.player = player
         self.game = game
         self.score = score
+        
+    @property
+    def score(self):
+        self.score
+    
+    @score.setter
+    def score(self, value):
+        if 1 <= value <= 5000:
+            self.score = value
+        else:
+            raise Exception('Score must be')
+    
+        
+    
